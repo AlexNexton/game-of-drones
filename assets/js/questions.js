b7156565ad05fcae7b3295ee214f824abe881d95
AlexNexton <cianoc13@gmail.com>
2021-03-07 17:28:34 +0000

    js,css: code complete

---
+++ assets/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "People traditionally wear green on St. Patrick's Day to avoid what?",
+    answer: "Getting Pinched",
+    options: [
+      "One Year Bad Luck",
+      "Getting Pinched",
+      "People not knowing they're Irish",
+      "Getting Kissed"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What nationality was St. Patrick?",
+    answer: "Scotish",
+    options: [
+      "Irish",
+      "German",
+      "Scotish",
+      "British"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which U.S. city dyes its river green annually to celebrate St. Patrick's Day?",
+    answer: "Chicago",
+    options: [
+      "New York",
+      "Boston",
+      "Los Angeles",
+      "Chicago"
+    ]
+  },
+    {
+    numb: 4,
+    question: "According to myth, when is the best time to sneak up on a leprechaun?",
+    answer: "While he's mending his shoes.",
+    options: [
+      "While he's mending his shoes.",
+      "When he's eating.",
+      "When he's taking a nap.",
+      "when he's counting gold."
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many pints of Guinness are consumed worldwide on St. Patrick's Day?",
+    answer: "13 million",
+    options: [
+      "1 million",
+      "6 million",
+      "13 million",
+      "500 thousand"
+    ]
+  },
+  // Hey hackathon team, you can uncomment the below code and add more questions of you like
+  // but remember you need to give the 'numb' value serialise like 6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
